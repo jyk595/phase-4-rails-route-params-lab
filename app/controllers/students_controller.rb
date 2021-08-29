2fa01bf7a008d120e37644b59f08d8f37a08fb06
jyk595 <jyk595@gmail.com>
2021-08-29 15:05:09 -0700

    commit tree 57fcf20cfaff733095ee5949ec8701c336965bf0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,19 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    students = Student.find_by(first_name: params[:first_name]) if params[:first_name].present?
+    students = Student.find_by(last_name: params[:last_name]) if params[:last_name].present?
     render json: students
   end
+
+  def show 
+    student_by_id = Student.find(params[:id])
+    render json: student_by_id
+  end
+
+  # def query
+  #   student_by_name = Student.find_by(first_name: params[:first_name])
+  #   render json: student_by_name
+  # end
 
 end
